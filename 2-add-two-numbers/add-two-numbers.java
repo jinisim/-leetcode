e69eb0984f0334acedb8b1fcb8809e90613e1447
jinhee lee <jinhee020928@naver.com>
2026-05-13 01:28:54 +0900

    Time: 1 ms (100.00%) | Memory: 46.7 MB (6.86%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -7,7 +7,8 @@
  *     ListNode(int val) { this.val = val; }
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
- */class Solution {
+ */
+ class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode(0);
         ListNode cur = dummy;
